1a6d2ab8b1060694f497f9f6bf38fb48a3768166
Gabriel Musat <gabriel.musat@dive.tech>
2021-01-15 23:34:14 +0100

    agent refactored

--- agents/agents/base/agent.py
+++ agents/agents/base/agent.py
@@ -17,6 +17,19 @@ from plotter import TensorBoard
 
 
 class Agent(ABC):
+    module_names: T.List[str] = []
+    modules: T.Dict[torch.nn.Module, T.Dict] = {}
+    healthy_callbacks: T.List[T.Callable[[Environment], None]] = []
+    step_callbacks: T.List[T.Callable[[TrainingStep], None]] = []
+    progress_callbacks: T.List[T.Callable[[TrainingProgress], bool]] = []
+    learning_callbacks: T.List[T.Callable[[LearningStep], None]] = []
+    model_wrappers: T.List[T.Callable[[torch.nn.Module], torch.nn.Module]] = []
+    save_path: T.Union[None, str] = None
+    summary_writer: T.Union[TensorBoard, None] = None
+    reward_record: T.List[float] = []
+    loss_record: T.List[float] = []
+    sample_input: T.Union[None, torch.Tensor] = None
+
     def __init__(self,
                  action_space: int,
                  hp: HyperParams,
@@ -38,133 +51,135 @@ class Agent(ABC):
         self.use_gpu: bool = use_gpu
         self.save_progress: bool = save_progress
         self.tensor_board_log: bool = tensor_board_log
-        self.healthy_callbacks: T.List[T.Callable[[Environment], None]] = []
-        self.step_callbacks: T.List[T.Callable[[TrainingStep], None]] = []
-        self.progress_callbacks: T.List[T.Callable[[TrainingProgress], bool]] = []
-        self.learning_callbacks: T.List[T.Callable[[LearningStep], None]] = []
-        self.model_wrappers: T.List[T.Callable[[torch.nn.Module], torch.nn.Module]] = []
-        self.save_path: T.Union[None, str] = None
-        self.summary_writer: T.Union[TensorBoard, None] = None
-        self.reward_record: T.List[float] = []
-        self.loss_record: T.List[float] = []
         self.link_replay_buffer()
         self.link_explorer()
         self.link_saver()
-        self.link_tensor_board()
+        self.link_tensorboard()
 
-    def link_tensor_board(self):
+    def tensorboard_log_training_progress(self, training_progress: TrainingProgress):
+        if self.summary_writer:
+            self.summary_writer.add_scalar("episode reward", training_progress.total_reward, training_progress.tries)
+
+        return False
+
+    def tensorboard_log_model_graph(self, env: Environment):
+        if self.save_path is None:
+            self.create_save_folder(env)
+
+        self.summary_writer = TensorBoard(self.save_path)
+        for attr, value in self.__dict__.items():
+            if isinstance(value, torch.nn.Module) and not attr.startswith("loss") and self.sample_input is not None:
+                self.summary_writer.add_graph(value, self.sample_input.to(self.device))
+                break
+
+    def tensorboard_log_random_explorer_add_epsilon(self, training_progress: TrainingProgress) -> bool:
+        if self.summary_writer:
+            self.summary_writer.add_scalar("random explorer Epsilon", self.explorer.epsilon, training_progress.tries)
+        return False
+    
+    def tensorboard_log_prioritized_replay_buffer_add_beta(self, training_progress: TrainingProgress):
+        if self.summary_writer:
+            self.summary_writer.add_scalar("prioritized replay buffer Beta", self.replay_buffer.beta, training_progress.tries)
+        return False
+
+    def forward_hook(self, module: torch.nn.Module, x: T.Tuple[torch.Tensor], y: torch.Tensor):
+        if self.sample_input is None:
+            self.sample_input = x[0]
+        if len(self.module_names) == 0:
+            for attr, value in self.__dict__.items():
+                if isinstance(value, torch.nn.Module) and not attr.startswith("loss"):
+                    self.module_names.append(attr)
+        if module not in self.modules:
+            self.modules[module] = {"name": self.module_names[len(self.modules)], "times": 0, "renders": 0}
+        if self.summary_writer and y.shape[0] > 1:
+            if self.modules[module]["times"] % 1000 == 0:
+                self.summary_writer.add_embedding(y,
+                                                  tag=self.modules[module]["name"],
+                                                  global_step=self.modules[module]["renders"])
+                self.modules[module]["renders"] += 1
+            self.modules[module]["times"] += 1
+
+    def link_tensorboard(self):
         if self.tensor_board_log:
             self.log.info("linking tensorboard callbacks...")
-
-            def log_progress(training_progress: TrainingProgress):
-                if self.summary_writer:
-                    self.summary_writer.add_scalar("episode reward", training_progress.total_reward, training_progress.tries)
-                return False
-
-            self.add_progress_callback(log_progress)
-
-            modules: T.Dict[torch.nn.Module, T.Dict] = {}
-            modules_names: T.List[str] = []
-
-            def forward_hook(module: torch.nn.Module, x: T.Tuple[torch.Tensor], y: torch.Tensor):
-                if len(modules_names) == 0:
-                    for attr, value in self.__dict__.items():
-                        if isinstance(value, torch.nn.Module) and not attr.startswith("loss"):
-                            modules_names.append(attr)
-                if module not in modules:
-                    modules[module] = {"name": modules_names[len(modules)], "times": 0, "renders": 0}
-                if self.summary_writer and y.shape[0] > 1:
-                    if modules[module]["times"] % 1000 == 0:
-                        self.summary_writer.add_embedding(y,
-                                                          tag=modules[module]["name"],
-                                                          global_step=modules[module]["renders"])
-                        modules[module]["renders"] += 1
-                    modules[module]["times"] += 1
+            self.add_progress_callback(self.tensorboard_log_training_progress)
+            self.add_healthy_callback(self.tensorboard_log_model_graph)
 
             def model_wrapper(model: torch.nn.Module):
-                model.register_forward_hook(forward_hook)
+                model.register_forward_hook(self.forward_hook)
                 return model
 
             self.model_wrappers.append(model_wrapper)
+            
+    def create_save_folder(self, env: Environment):
+        base = os.environ.get("SAVE_DIR", "data")
+        if base.endswith("/"):
+            base = base[:-1]
+
+        agent = self.get_self_class_name()
+        today = str(datetime.datetime.now().date())
+        now = str(datetime.datetime.now().time().strftime("%H:%M:%S"))
+        folder = ""
+        for sub_folder in [base, agent, type(env).__name__, today, now]:
+            folder = os.path.join(folder, sub_folder)
+            if not os.path.isdir(folder):
+                self.log.info(f"folder {folder} does not exists, creating it...")
+                os.mkdir(folder)
+        self.save_path = folder
+        self.log.info(f"all save folders created: {folder}")
+
+    def save_agent_info_callback(self, env: Environment):
+        self.log.info("initializing save callback triggered")
+        if self.save_path is None:
+            self.create_save_folder(env)
+        agent_info_path = os.path.join(self.save_path, "agent.json")
+        json.dump(self.get_info(), open(agent_info_path, "w"), indent=4)
+        self.log.info("agent.json created correctly")
+
+    def save_training_progress_callback(self, training_progress: TrainingProgress) -> bool:
+        self.log.debug("save callback triggered")
+        if self.save_path is None:
+            self.log.info("progress saving aborted, agent cannot be considered healthy yet")
+            return False
+        folder_checkpoints = os.path.join(self.save_path, "checkpoints")
+        if not os.path.isdir(folder_checkpoints):
+            os.mkdir(folder_checkpoints)
+
+        folder_checkpoints_checkpoint = os.path.join(folder_checkpoints, str(time.time()) + ".json")
+        json.dump(training_progress.__dict__, open(folder_checkpoints_checkpoint, "w"))
+        self.log.debug("checkpoint saved correctly")
+        return False
 
     def link_saver(self):
         if self.save_progress:
             self.log.info("linking saving callbacks...")
-
-            def init_save_callback(env: Environment):
-                self.log.info("initializing save callback triggered")
-                base = os.environ.get("SAVE_DIR", "data")
-                if base.endswith("/"):
-                    base = base[:-1]
-
-                agent = self.get_self_class_name()
-                today = str(datetime.datetime.now().date())
-                now = str(datetime.datetime.now().time().strftime("%H:%M:%S"))
-                folder = ""
-                for sub_folder in [base, agent, type(env).__name__, today, now]:
-                    folder = os.path.join(folder, sub_folder)
-                    if not os.path.isdir(folder):
-                        self.log.info(f"folder {folder} does not exists, creating it...")
-                        os.mkdir(folder)
-                self.log.info(f"all save folders created: {folder}")
-                self.save_path = folder
-                if self.tensor_board_log:
-                    self.summary_writer = TensorBoard(folder)
-                    for attr, value in self.__dict__.items():
-                        if isinstance(value, torch.nn.Module) and not attr.startswith("loss"):
-                            self.summary_writer.add_graph(value, self.preprocess(env.last_s).to(self.device))
-                            break
-                agent_info_path = os.path.join(folder, "agent.json")
-                json.dump(self.get_info(), open(agent_info_path, "w"), indent=4)
-                self.log.info("agent.json created correctly")
-
-            self.add_healthy_callback(init_save_callback)
-
-            def checkpoint_save_callback(training_progress: TrainingProgress):
-                self.log.debug("save callback triggered")
-                if self.save_path is None:
-                    self.log.info("progress saving aborted, agent cannot be considered healthy yet")
-                    return
-                folder_checkpoints = os.path.join(self.save_path, "checkpoints")
-                if not os.path.isdir(folder_checkpoints):
-                    os.mkdir(folder_checkpoints)
-
-                folder_checkpoints_checkpoint = os.path.join(folder_checkpoints, str(time.time())+".json")
-                json.dump(training_progress.__dict__, open(folder_checkpoints_checkpoint, "w"))
-                self.log.debug("checkpoint saved correctly")
-                return False
-
-            self.add_progress_callback(checkpoint_save_callback)
+            self.add_healthy_callback(self.create_save_folder)
+            self.add_healthy_callback(self.save_agent_info_callback)
+            self.add_progress_callback(self.save_training_progress_callback)
             self.log.info("progress callbacks linked correctly")
         else:
             self.log.info("progress is not going to be saved")
+
+    def prioritized_replay_buffer_update_priorities(self, learning_step: LearningStep):
+        self.log.debug(f"update priorities for {type(self.replay_buffer).__name__} triggered")
+        self.replay_buffer.update_priorities(
+            [e.index for e in learning_step.batch],
+            [abs(x - y) + 1e-7 for x, y in zip(learning_step.x, learning_step.y)]
+        )
+
+    def prioritized_replay_buffer_increase_beta(self, _: TrainingStep):
+        self.log.debug(f"increase beta for {type(self.replay_buffer).__name__} triggered")
+        self.replay_buffer.increase_beta()
 
     def link_replay_buffer(self):
         if isinstance(self.replay_buffer, NStepsPrioritizedReplayBuffer):
             self.log.info(f"linking {type(self.replay_buffer).__name__}...")
 
-            def update_prioritized_replay_buffer_priorities(learning_step: LearningStep):
-                self.log.debug(f"update priorities for {type(self.replay_buffer).__name__} triggered")
-                self.replay_buffer.update_priorities(
-                    [e.index for e in learning_step.batch],
-                    [abs(x - y) + 1e-7 for x, y in zip(learning_step.x, learning_step.y)]
-                )
-
-            self.add_learn_callback(update_prioritized_replay_buffer_priorities)
-
-            def increase_prioritized_replay_buffer_beta(_: TrainingStep):
-                self.log.debug(f"increase beta for {type(self.replay_buffer).__name__} triggered")
-                self.replay_buffer.increase_beta()
-
-            self.add_step_callback(increase_prioritized_replay_buffer_beta)
+            self.add_learn_callback(self.prioritized_replay_buffer_update_priorities)
+            self.add_step_callback(self.prioritized_replay_buffer_increase_beta)
 
             if self.tensor_board_log:
-                def add_beta_to_tensorboard(training_progress: TrainingProgress):
-                    if self.summary_writer:
-                        self.summary_writer.add_scalar("prioritized replay buffer Beta", self.replay_buffer.beta, training_progress.tries)
-                    return False
-
-                self.add_progress_callback(add_beta_to_tensorboard)
+                self.add_progress_callback(self.tensorboard_log_prioritized_replay_buffer_add_beta)
 
             new_gamma = self.hp.gamma ** self.replay_buffer.rp.n_step
             self.log.info(f"refactoring gamma for {type(self.replay_buffer).__name__}: {self.gamma} -> {new_gamma}")
@@ -173,24 +188,26 @@ class Agent(ABC):
         else:
             self.log.info(f"{type(self.replay_buffer).__name__} replay buffer does not need linking")
 
+    def random_explorer_decay_epsilon(self, _: TrainingStep):
+        self.log.debug(f"decay epsilon for {type(self.explorer).__name__} triggered")
+        self.explorer.decay()
+
+    def noisy_explorer_reset_noise(self, training_step: TrainingStep):
+        self.log.debug(f"reset noise for {type(self.explorer).__name__} triggered")
+        if training_step.i % self.explorer.ep.reset_noise_every == 0:
+            for attr, value in self.__dict__.items():
+                if isinstance(value, torch.nn.Module):
+                    for i, layer in enumerate(value.modules()):
+                        if isinstance(layer, NoisyLinear):
+                            self.log.debug(f"layer {i} for attribute {attr} is noisy, noise reset")
+                            layer.reset_noise()
+
     def link_explorer(self):
         if isinstance(self.explorer, RandomExplorer):
             self.log.info(f"linking {type(self.explorer).__name__}...")
-
-            def decay_random_explorer_epsilon(_: TrainingStep):
-                self.log.debug(f"decay epsilon for {type(self.explorer).__name__} triggered")
-                self.explorer.decay()
-
-            self.add_step_callback(decay_random_explorer_epsilon)
-
+            self.add_step_callback(self.random_explorer_decay_epsilon)
             if self.tensor_board_log:
-                def add_epsilon_to_tensorboard(training_progress: TrainingProgress) -> bool:
-                    if self.summary_writer:
-                        self.summary_writer.add_scalar("random explorer Epsilon", self.explorer.epsilon, training_progress.tries)
-                    return False
-
-                self.add_progress_callback(add_epsilon_to_tensorboard)
-
+                self.add_progress_callback(self.tensorboard_log_random_explorer_add_epsilon)
             self.log.info(f"{type(self.explorer).__name__} linked correctly")
 
         elif isinstance(self.explorer, NoisyExplorer):
@@ -201,18 +218,7 @@ class Agent(ABC):
                 return self.explorer.wrap_model(model)
 
             self.model_wrappers.append(noisy_linear_model_factory)
-
-            def reset_noisy_explorer_noise(training_step: TrainingStep):
-                self.log.debug(f"reset noise for {type(self.explorer).__name__} triggered")
-                if training_step.i % self.explorer.ep.reset_noise_every == 0:
-                    for attr, value in self.__dict__.items():
-                        if isinstance(value, torch.nn.Module):
-                            for i, layer in enumerate(value.modules()):
-                                if isinstance(layer, NoisyLinear):
-                                    self.log.debug(f"layer {i} for attribute {attr} is noisy, noise reset")
-                                    layer.reset_noise()
-
-            self.add_step_callback(reset_noisy_explorer_noise)
+            self.add_step_callback(self.noisy_explorer_reset_noise)
 
         else:
             self.log.info(f"{type(self.explorer).__name__} explorer does not need linking")
--- testing/cart_pole/train_a2c_cart_pole.py
+++ testing/cart_pole/train_a2c_cart_pole.py
@@ -8,10 +8,10 @@ from agents.explorers import NoisyExplorer, NoisyExplorerParams
 from environments import CartPole
 
 
-AGENT_PARAMS = ACHyperParams(c_lr=0.01, a_lr=0.03, gamma=0.97)
+AGENT_PARAMS = ACHyperParams(c_lr=0.01, a_lr=0.01, gamma=0.97)
 TRAINING_PARAMS = TrainingParams(learn_every=1, batch_size=128, episodes=300)
 NOISY_EXPLORER_PARAMS = NoisyExplorerParams(extra_layers=[], std_init=0.5, reset_noise_every=1)
-REPLAY_BUFFER_PARAMS = NStepPrioritizedReplayBufferParams(max_len=1000, gamma=AGENT_PARAMS.gamma, n_step=5, alpha=0.6,
+REPLAY_BUFFER_PARAMS = NStepPrioritizedReplayBufferParams(max_len=20000, gamma=AGENT_PARAMS.gamma, n_step=5, alpha=0.6,
                                                           init_beta=0.4, final_beta=1.0, increase_beta=1e-4)
 
 USE_MONTE_CARLO = True
@@ -24,11 +24,9 @@ class CustomActionEstimator(torch.nn.Module):
     def __init__(self, in_size: int):
         super(CustomActionEstimator, self).__init__()
         self.linear1 = torch.nn.Linear(in_size, 10)
-        self.linear2 = torch.nn.Linear(10, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = F.relu(self.linear1(x))
-        return F.relu(self.linear2(x))
+        return F.relu(self.linear1(x))
 
 
 class CustomActorCriticAgent(AgentParentClass):
